24b9cc890174c68d7431340a37b07ed2aa2b193b
rebaza13 <rebazking13v@gmail.com>
2025-09-02 23:40:59 +0300

    Refactor header and hero styles; update layout for improved responsiveness and aesthetics
    - Enhanced header styles with responsive font sizes and margins.
    - Adjusted menu toggle visibility for better user experience on larger screens.
    - Updated hero section styles for improved layout and alignment.
    - Changed background of the body to a gradient for a modern look.
    - Localized menu items in the header to Kurdish language.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,86 +1,83 @@
 <template>
 
-    <div>
-      <header class="header">
-         <div id="menuToggle">
-    <!--
+  <div>
+    <header class="header">
+      <div id="menuToggle">
+        <!--
     A fake / hidden checkbox is used as click reciever,
     so you can use the :checked selector on it.
     -->
-    <input type="checkbox" id="menuCheckbox" />
-    
-    <!--
+        <input type="checkbox" id="menuCheckbox" />
+
+        <!--
     Some spans to act as a hamburger.
     
     They are acting like a real hamburger,
     not that McDonalds stuff.
     -->
-    <span></span>
-    <span></span>
-    <span></span>
-    
-    <!--
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <!--
     Too bad the menu has to be inside of the button
     but hey, it's pure CSS magic.
     -->
-    <ul id="menu">
-      <!-- 
+        <ul id="menu">
+          <!-- 
       We can use a label here to close upon click (when doing same page navigation), this
       does require a slight bit of JS.
       -->
-      <li>
-        <a href="#">
-          <label for="menuCheckbox" onclick="this.parentNode.click();">Home</label>
-        </a>
-      </li>
-      <li>
-        <a href="#about">
-          <label for="menuCheckbox" onclick="this.parentNode.click();">About</label>
-        </a>
-      </li>
-      
-      <!-- These just close the menu -->
-      <li><label for="menuCheckbox"><a>Info</a></label></li>
-      <li><label for="menuCheckbox"><a>Contact</a></label></li>
-      
-      <!-- Or just use regular URLs -->
-      <li>
-        <a href="https://erikterwan.com/" target="_blank">Show me more</a>
-      </li>
-    </ul>
+          <li>
+            <a href="#">
+              <label for="menuCheckbox" onclick="this.parentNode.click();">سەرەکی</label>
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              <label for="menuCheckbox" onclick="this.parentNode.click();">دەربارە</label>
+            </a>
+          </li>
+
+          <!-- These just close the menu -->
+          <li><label for="menuCheckbox"><a>زانیاری</a></label></li>
+          <li><label for="menuCheckbox"><a>پەیوەندی</a></label></li>
+
+         
+        </ul>
+      </div>
+      <div class="header__logo">
+        عەرەبی لەگەڵ <span class="header__logo-hilighted">ئازاد</span>
+      </div>
+      <div class="header__menu">
+        <ul class="header__list">
+          <li class="header__item">
+            <a href="/" class="header__link">سەرەکی</a>
+          </li>
+          <li class="header__item">
+            <a href="/" class="header__link">وانەکان</a>
+          </li>
+          <li class="header__item">
+            <a href="/" class="header__link">ماددەکان</a>
+          </li>
+          <li class="header__item">
+            <a href="/" class="header__link">تاقیکردنەوی ئاست</a>
+          </li>
+
+        </ul>
+      </div>
+      <div class="header__buttons">
+        <button class="header__button">
+          <a href="/" class="header__link">خۆتۆمارکردن</a>
+        </button>
+        <button class="header__button">
+          <a href="/" class="header__link">داخڵ بوون</a>
+        </button>
+
+      </div>
+    </header>
+    <slot />
   </div>
-        <div class="header__logo">
-          عەرەبی لەگەڵ <span class="header__logo-hilighted">ئازاد</span>
-        </div>
-        <div class="header__menu">
-          <ul class="header__list">
-            <li class="header__item">
-              <a href="/" class="header__link">سەرەکی</a>
-            </li>
-            <li class="header__item">
-              <a href="/" class="header__link">وانەکان</a>
-            </li>
-            <li class="header__item">
-              <a href="/" class="header__link">ماددەکان</a>
-            </li>
-            <li class="header__item">
-              <a href="/" class="header__link">تاقیکردنەوی  ئاست</a>
-            </li>
 
-          </ul>
-        </div>
-        <div class="header__buttons">
-          <button class="header__button">
-            <a href="/" class="header__link">خۆتۆمارکردن</a>
-          </button>
-          <button class="header__button">
-            <a href="/" class="header__link">داخڵ بوون</a>
-          </button>
-       
-        </div>
-      </header>
-      <slot />
-    </div>
 
- 
 </template>
